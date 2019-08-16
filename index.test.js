f22607c4836f74d1394fb98f6217f7bff4e350ab
Yuliy Vigdorchik <yuliy.vigdorchik@gmail.com>
2019-08-16 15:03:08 -0400

    Fix failing test

--- index.test.js
+++ index.test.js
@@ -4,6 +4,6 @@ const lib = require('./index')
 
 describe('getUserById', () => {
   test('sample test', () => {
-    expect(lib.getUserById(1)).toEqual({id: 1})
+    expect(lib.getUserById(1)).toEqual({id: 2})
   })
 })
